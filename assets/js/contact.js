58baccb8df02dbbc6b5de5eeb1f2bee7ab16a1a0
safaomnia <soulisafa47@gmail.com>
2024-10-04 21:41:17 +0100

    feat: add check mark when the value is correct

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -7,10 +7,17 @@ $(document).ready(function () {
   const $message = $('#message');
   const $subject = $('#subject');
   const regExEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const regExPhone = /^[234579]\d{1}\s?\d{3}\s?\d{3}$/;
+  const regExPhone = /^[234579]\d{1}\s?\d{3}\s?\d{3}$/; // Adjust based on phone number format
   const $submitElement = $('.form__group--submit');
   let validSubmit = false;
 
+  // Phone number input mask
+  $("#phone").inputmask("99-999-999", {
+    placeholder: "_",
+    clearMaskOnLostFocus: true
+  });
+
+  // Validation function
   function validation() {
     validSubmit = true;
     validateField($firstName, 'Please enter a valid name');
@@ -19,45 +26,67 @@ $(document).ready(function () {
     validateField($message, 'Please enter a message so I can help you');
   }
 
+  // General field validation
   function validateField($field, errorMessage) {
     if ($field.val().trim() === '') {
+      $field.removeClass('valid');
       $field.addClass('invalid');
+      removeSuccessIcon($field);
       addErrorIcon($field, errorMessage);
       validSubmit = false;
     } else {
-      $field.removeClass('invalid');
-      removeErrorIcon($field);
+      if ($field.hasClass('invalid')) {
+        $field.removeClass('invalid');
+        removeErrorIcon($field);
+        $field.addClass('valid');
+        addSuccessIcon($field);
+      }
     }
   }
 
+  // Email validation
   function validateEmail($field, errorMessage) {
     const emailValue = $field.val().trim();
     if (emailValue === '' || !regExEmail.test(emailValue)) {
+      $field.removeClass('valid');
       $field.addClass('invalid');
+      removeSuccessIcon($field);
       addErrorIcon($field, errorMessage);
       validSubmit = false;
     } else {
-      $field.removeClass('invalid');
-      removeErrorIcon($field);
+      if ($field.hasClass('invalid')) {
+        $field.removeClass('invalid');
+        removeErrorIcon($field);
+        $field.addClass('valid');
+        addSuccessIcon($field);
+      }
     }
   }
 
+  // Phone validation
   function validatePhone($field, errorMessage) {
-    const phoneValue = $field.val().trim();
+    const phoneValue = $field.val().trim().replace(/-/g, '');
     if (phoneValue !== '' && !regExPhone.test(phoneValue)) {
+      $field.removeClass('valid');
       $field.addClass('invalid');
+      removeSuccessIcon($field);
       addErrorIcon($field, errorMessage);
       validSubmit = false;
     } else {
-      $field.removeClass('invalid');
-      removeErrorIcon($field);
+      if ($field.hasClass('invalid')) {
+        $field.removeClass('invalid');
+        removeErrorIcon($field);
+        $field.addClass('valid');
+        addSuccessIcon($field);
+      }
     }
   }
 
+  // Add error icon to field
   function addErrorIcon($input, errorMessage) {
     if ($input.siblings('.form__icon').length === 0) {
       $input.after(`
-        <span class="hint--top-left form__icon" data-hint="${errorMessage}">
+        <span class="hint--top-left hint--error form__icon" data-hint="${errorMessage}">
           <i class="icon icon--exclamation"></i>
         </span>
       `);
@@ -66,13 +95,35 @@ $(document).ready(function () {
     }
   }
 
+  // Remove error icon from field
   function removeErrorIcon($input) {
     $input.siblings('.form__icon').remove();
   }
 
+  function addSuccessIcon($input) {
+    if ($input.siblings('.form__icon').length === 0) {
+      $input.after(`
+        <span class="hint--top-left hint--success form__icon" data-hint="looks good!">
+          <i class="icon icon--check"></i>
+        </span>
+      `);
+    } else {
+      $input.siblings('.form__icon').attr('data-hint', 'looks good!').show();
+    }
+  }
+
+  function removeSuccessIcon($input) {
+    $input.siblings('.form__icon').remove();
+  }
+
+
+  // Form submission
   $form.on('submit', function (e) {
     e.preventDefault();
+    // $('.bounce').removeClass('bounce');
+    console.log($('.bounce').length)
     $('.bounce').removeClass('bounce');
+    console.log($('.bounce').length)
     validation();
 
     if (validSubmit) {
@@ -82,6 +133,7 @@ $(document).ready(function () {
     }
   });
 
+  // Handle email submission via EmailJS
   function handleEmailSubmission() {
     $submitElement.find('.cta__btn').addClass('disabled');
     $submitElement.append('<p class="form__loading">Sending<span class="dots"></span></p>');
@@ -102,6 +154,7 @@ $(document).ready(function () {
       .catch(handleError);
   }
 
+  // Success handler
   function handleSuccess(response) {
     $submitElement.find('.form__loading').remove();
     $submitElement.append('<p class="form__success form__result"><i class="icon icon--check"></i> Your message has been sent. Thank you! We will get back to you shortly.</p>');
@@ -112,6 +165,7 @@ $(document).ready(function () {
     $form[0].reset();
   }
 
+  // Error handler
   function handleError(error) {
     $submitElement.find('.form__loading').remove();
     $submitElement.append('<p class="form__error form__result"><i class="icon icon--exclamation"></i> Something went wrong. Please reload the page and try again.</p>');
@@ -122,10 +176,12 @@ $(document).ready(function () {
     console.error('Email sending failed:', error);
   }
 
+  // Highlight invalid fields
   function highlightInvalidFields() {
     const $firstInvalid = $('.form__input.invalid').eq(0);
+    $firstInvalid.attr('autocomplete', 'off');
     $firstInvalid.addClass('bounce');
     $firstInvalid.next('[class*="hint"]').addClass('bounce');
-    $firstInvalid.on('input', validation);
+    $firstInvalid.on('input', validation); // Validate only on blur event
   }
 });
