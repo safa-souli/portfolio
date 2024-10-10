a32155cb75e056a9b42396c0b8451b560c3c8aaf
safa-souli <soulisafa47@gmail.com>
2024-10-10 23:36:18 +0100

    fix: path sw

--- assets/js/app.js
+++ assets/js/app.js
@@ -794,7 +794,7 @@ $(document).ready(function () {
     if ('serviceWorker' in navigator) {
       console.debug('Service Worker is supported');
       window.addEventListener('load', () => {
-        navigator.serviceWorker.register('../sw.js', { scope: '/' })
+        navigator.serviceWorker.register('https://safa-souli.github.io/portfolio/sw.js', { scope: '/portfolio/' })
           .then((registration) => {
             console.debug('Service Worker registered with scope:', registration.scope);
           })
